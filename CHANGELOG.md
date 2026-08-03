8cc672f7b92f584ccc7f45e36bd53a96e9ef5520
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2026-08-03 14:34:06 +0200

    #gg: Finish development of version 1.4.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 1.4.2 - 2026-08-03
 
 ### Added
 
--- lib/src/gg_status_printer_version.dart
+++ lib/src/gg_status_printer_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_status_printer` package.
-const String ggStatusPrinterVersion = '1.4.1';
+const String ggStatusPrinterVersion = '1.4.2';
